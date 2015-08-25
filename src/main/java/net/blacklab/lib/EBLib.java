8b3195381b6d82a6070636e0b42289ecad8f4cb7
Verclene <xdev58bo@gmail.com>
2015-08-25 23:02:41 +0900

    EL1 Build 4

--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -1,8 +1,19 @@
 package net.blacklab.lib;
 
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = "net.blacklab.lib",name="EBLib",version="EL1 Build 3")
+@Mod(modid = "net.blacklab.lib",name="EBLib",version=EBLib.VERSION)
 public class EBLib {
+	
+	public static final String VERSION="EL1 Build 4";
+	public static final int VERSION_CODE=1;
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		FMLCommonHandler.instance().bus().register(new EventHook());
+	}
 
 }
---
+++ src/main/java/net/blacklab/lib/EventHook.java
@@ -0,0 +1,35 @@
+package net.blacklab.lib;
+
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+
+public class EventHook {
+
+	public class RunThread extends Thread{
+		public PlayerEvent.PlayerLoggedInEvent e;
+		
+		public RunThread(PlayerEvent.PlayerLoggedInEvent ev){
+			e = ev;
+		}
+		
+		public void run(){
+			Version.VersionData v = Version.getLatestVersion("http://mc.el-blacklab.net/eblibversion.txt");
+			if(EBLib.VERSION_CODE < v.code){
+				//バージョンが古い
+				// TODO これメイドのAvatarキャッチしない？
+				try{
+					//別スレッドから使えるんかい
+					e.player.addChatMessage(new ChatComponentText("[EBLib]New Version Avaliable : "+v.name));
+					e.player.addChatMessage(new ChatComponentText("[EBLib]Go to : http://el-blacklab.net/"));
+				}catch(Exception e){}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerLogin(PlayerLoggedInEvent event){
+		new RunThread(event).run();
+	}
+}
