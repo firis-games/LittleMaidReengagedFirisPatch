1f9745a082b0197b193eae21d5a2bcd33eccd3b7
Verclene <xdev58bo@gmail.com>
2015-08-25 22:25:00 +0900

    Refactor from LMMNX

--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -2,7 +2,7 @@ package net.blacklab.lib;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "net.blacklab.lib",name="EBLib",version="EL1 Build 2")
+@Mod(modid = "net.blacklab.lib",name="EBLib",version="EL1 Build 3")
 public class EBLib {
 
 }
