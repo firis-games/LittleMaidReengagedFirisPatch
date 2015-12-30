d7df29de914bd423381a26ceb7252daaaff5259c
Verclene <xdev58bo@gmail.com>
2015-12-30 20:41:13 +0900

    Update acceptedMinecraftVersion

--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -5,12 +5,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = "net.blacklab.lib",name="EBLib",version=EBLib.VERSION, acceptedMinecraftVersions="[1.8,1.8.8]")
+@Mod(modid = "net.blacklab.lib",name="EBLib",version=EBLib.VERSION, acceptedMinecraftVersions="[1.8,1.8.9]")
 public class EBLib {
 
-	public static final String VERSION="3.0.9";
-	public static final String VERSION_FORSITE="EL3 Build 9";
-	public static final int VERSION_CODE=4;
+	public static final String VERSION="3.1.10";
+	public static final String VERSION_FORSITE="EL3 Build 10";
+	public static final int VERSION_CODE=5;
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event){
