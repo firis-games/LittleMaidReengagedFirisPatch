6aed9338805283b4f54892e3ba8617bea18aafe3
Verclene <xdev58bo@gmail.com>
2016-02-04 19:44:41 +0900

    raise version

--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -11,9 +11,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = "net.blacklab.lib",name="EBLib",version=EBLib.VERSION, acceptedMinecraftVersions="[1.8,1.8.9]")
 public class EBLib {
 
-	public static final String VERSION="3.2.12";
-	public static final String VERSION_FORSITE="EL3 Build 12";
-	public static final int VERSION_CODE=6;
+	public static final String VERSION="3.3.13";
+	public static final String VERSION_FORSITE="EL3 Build 13";
+	public static final int VERSION_CODE=7;
 	
 	public static final VersionData currentVersion = new VersionData(VERSION_CODE, VERSION, VERSION_FORSITE);
 	public static VersionData latestVersion = new VersionData(1, "1.0.1", "EL1");
