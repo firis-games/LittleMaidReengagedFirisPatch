b6273c012fa137b2859a064fdc542b29a469d701
Verclene <xdev58bo@gmail.com>
2016-04-07 01:04:39 +0900

    Modify mcmod.info; Add logo

--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "net.blacklab.lib",name="EBLib",version=EBLib.VERSION, acceptedMinecraftVersions="[1.9,)")
+@Mod(modid = "net.blacklab.lib",name="EBLib",version=EBLib.VERSION, acceptedMinecraftVersions="[1.9,1.9.100)")
 public class EBLib {
 
 	public static final String VERSION="5.1.0.1";
