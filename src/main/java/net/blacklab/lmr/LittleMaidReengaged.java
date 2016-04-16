d47220928bb425e4801e968fd96e5c53267c6964
Verclene <xdev58bo@gmail.com>
2016-04-17 01:57:30 +0900

    (7.1.4.7)Update versions info

--- src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
+++ src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
@@ -60,7 +60,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class LittleMaidReengaged {
 
 	public static final String DOMAIN = "lmreengaged";
-	public static final String VERSION = "7.1.2.4";
+	public static final String VERSION = "7.1.4.7";
 	public static final String ACCEPTED_MCVERSION = "[1.9,1.9.100)";
 	public static final int VERSION_CODE = 1;
 	public static final String DEPENDENCIES = "required-after:Forge@[1.9-12.16.0.1819,);"
