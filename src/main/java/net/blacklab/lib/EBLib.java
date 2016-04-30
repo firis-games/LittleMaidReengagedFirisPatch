7403f1adeb1871f3de7af6ca2d0f0ca241572a41
Verclene <xdev58bo@gmail.com>
2016-04-30 23:28:29 +0900

    5.2.0.3: Migrate version check to Forge updateJSON

--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -1,30 +1,26 @@
 package net.blacklab.lib;
 
-import net.blacklab.lib.version.Version;
-import net.blacklab.lib.version.Version.VersionData;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "net.blacklab.lib",name="EBLib",version=EBLib.VERSION, acceptedMinecraftVersions="[1.9,1.9.100)")
+@Mod(
+		modid = "net.blacklab.lib",
+		name="EBLib",
+		version=EBLib.VERSION,
+		acceptedMinecraftVersions="[1.9,1.9.100)",
+		updateJSON = "http://mc.el-blacklab.net/eblib-version.json")
 public class EBLib {
 
-	public static final String VERSION="5.2.0.2";
-	public static final int VERSION_CODE=8;
-	
-	public static final VersionData currentVersion = new VersionData(VERSION_CODE, VERSION, VERSION);
-	public static VersionData latestVersion = new VersionData(1, "1.0.1", "EL1");
+	public static final String VERSION="5.2.0.3";
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		latestVersion = Version.getLatestVersion("http://mc.el-blacklab.net/eblibversion.txt");
 	}
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event){
-		MinecraftForge.EVENT_BUS.register(new EventHook());
 	}
 
 }
