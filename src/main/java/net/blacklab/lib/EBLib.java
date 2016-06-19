b1f4927ec16609194e3b76e0ed75edba0873c5de
Verclene <xdev58bo@gmail.com>
2016-06-19 23:47:38 +0900

    Revert 5.2.1.4

--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 		updateJSON = "http://mc.el-blacklab.net/eblib-version.json")
 public class EBLib {
 
-	public static final String VERSION="5.2.1.4";
+	public static final String VERSION="5.2.0.3";
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
