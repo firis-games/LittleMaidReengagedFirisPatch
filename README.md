e59059de992837bf6695f5140ced11e70ac1a1be
Verclene <xdev58bo@gmail.com>
2016-06-26 23:46:28 +0900

    6.0.1.4: Expand launchable range(1.9/1.9.4/1.10); Modify README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# EBLib-1.8
+# EBLib
 A common library for Verclene's mods.
 
-This is common library of Verclene's mods.
+This is common library of Verclene's mods. for Minecraft 1.9/1.9.4/1.10
--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -9,12 +9,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 		modid = "net.blacklab.lib",
 		name="EBLib",
 		version=EBLib.VERSION,
-		acceptedMinecraftVersions="[1.9,1.9.100)",
+		acceptedMinecraftVersions="[1.9,1.10]",
 		updateJSON = "http://mc.el-blacklab.net/eblib-version.json")
 public class EBLib {
 
-	public static final String VERSION="5.2.0.3";
-	
+	public static final String VERSION="6.0.1.4";
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 	}
