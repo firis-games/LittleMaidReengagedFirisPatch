f0bf9b956ce98d54d15b63f26a43053aa73b6923
Verclene <xdev58bo@gmail.com>
2017-09-03 21:41:36 +0900

    Revert "8.1.3.105: Make EntityModeManager independent from ManagerBase"
    This reverts commit 5fa8090860ff069c67dd78173d900be503444d98.

--- src/main/java/net/blacklab/lmr/util/manager/EntityModeManager.java
+++ src/main/java/net/blacklab/lmr/util/manager/EntityModeManager.java
@@ -3,24 +3,82 @@ package net.blacklab.lmr.util.manager;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.blacklab.lib.classutil.FileClassUtil;
+import net.blacklab.lmr.LittleMaidReengaged;
 import net.blacklab.lmr.entity.EntityLittleMaid;
 import net.blacklab.lmr.entity.mode.EntityModeBase;
 import net.blacklab.lmr.util.FileList;
 import net.blacklab.lmr.util.coremod.Transformer;
 
-public class EntityModeManager  {
+public class EntityModeManager extends ManagerBase {
 
 	public static final String prefix = "EntityMode";
+	private List<EntityModeBase> maidModeList = new ArrayList<EntityModeBase>();
 
+	private static EntityModeManager instance = new EntityModeManager();
+	
 	public static void init() {
 		// 特定名称をプリフィックスに持つmodファイをを獲得
 		FileList.getModFile("EntityMode", prefix);
 	}
 	
+	public EntityModeManager() {
+		maidModeList = new ArrayList<>();
+	}
+	
+	public static EntityModeManager getInstance() {
+		return instance;
+	}
+	
+	public void loadEntityMode() {
+		getInstance().load();
+	}
+	
+	@Override
+	protected String getPreFix() {
+		return prefix;
+	}
+
+	@Deprecated
+	@Override
+	protected boolean append(Class pclass) {
+		// プライオリティー順に追加
+		// ソーター使う？
+		if (!EntityModeBase.class.isAssignableFrom(pclass)) {
+			return false;
+		}
+		
+		try {
+			EntityModeBase lemb = null;
+			lemb = (EntityModeBase)pclass.getConstructor(EntityLittleMaid.class).newInstance((EntityLittleMaid)null);
+			lemb.init();
+			
+			//既存
+			if(maidModeList.contains(lemb)) return false;
+			
+			if (maidModeList.isEmpty() || lemb.priority() >= maidModeList.get(maidModeList.size() - 1).priority()) {
+				maidModeList.add(lemb);
+			} else {
+				for (int li = 0; li < maidModeList.size(); li++) {
+					if (lemb.priority() < maidModeList.get(li).priority()) {
+						maidModeList.add(li, lemb);
+						break;
+					}
+				}
+			}
+
+			return true;
+		} catch (Exception e) {
+		} catch (Error e) {
+		}
+
+		return false;
+	}
+
 	/**
 	 * AI追加用のリストを獲得。 
 	 */
-	public static List<EntityModeBase> getModeList(EntityLittleMaid pentity) {
+	public List<EntityModeBase> getModeList(EntityLittleMaid pentity) {
 		List<EntityModeBase> llist = new ArrayList<EntityModeBase>();
 		for (String lmode : Transformer.getEntityModeList()) {
 			try {
@@ -34,6 +92,17 @@ public class EntityModeManager  {
 		}
 		
 		return llist;
+	}
+
+	/**
+	 * ロードされているモードリストを表示する。
+	 */
+	@Deprecated
+	public void showLoadedModes() {
+		LittleMaidReengaged.Debug("Loaded Mode lists(%d)", maidModeList.size());
+		for (EntityModeBase lem : maidModeList) {
+			LittleMaidReengaged.Debug("%04d : %s", lem.priority(), lem.getClass().getSimpleName());
+		}
 	}
 
 }
