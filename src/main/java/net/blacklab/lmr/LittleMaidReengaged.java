09be33ce8ace5f32ce4a3df2597594e866be0368
firis <firis.games.lapismoonlight@gmail.com>
2020-01-03 19:56:17 +0900

    ver.fp.014

--- src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
+++ src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
@@ -72,7 +72,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class LittleMaidReengaged {
 
 	public static final String DOMAIN = "lmreengaged";
-	public static final String VERSION = "8.1.6.141.fp.013";
+	public static final String VERSION = "8.1.6.141.fp.014";
 	public static final String ACCEPTED_MCVERSION = "[1.12.2]";
 	public static final String DEPENDENCIES = "required-after:forge@[1.12.2-14.23.5.2768,);";
 
