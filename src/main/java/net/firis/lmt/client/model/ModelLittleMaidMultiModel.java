1b103dd68011c616443c3a0544f3b7bf1761c0cf
firis <firis.games.lapismoonlight@gmail.com>
2020-03-19 22:48:36 +0900

    EntityPlayer用IModelCaps追加
    ・EntityPlayerからマルチモデル描画に必要なパラメータを取得する
    ・パラメータはすべてIModelCaps内で処理する

--- src/main/java/net/firis/lmt/client/model/ModelLittleMaidMultiModel.java
+++ src/main/java/net/firis/lmt/client/model/ModelLittleMaidMultiModel.java
@@ -1,17 +1,14 @@
 package net.firis.lmt.client.model;
 
-import net.blacklab.lmr.entity.maidmodel.IModelCaps;
 import net.blacklab.lmr.entity.maidmodel.ModelMultiBase;
 import net.blacklab.lmr.entity.maidmodel.TextureBox;
-import net.blacklab.lmr.util.EntityCapsLiving;
 import net.blacklab.lmr.util.manager.ModelManager;
 import net.firis.lmt.client.renderer.RendererMaidPlayerMultiModel;
+import net.firis.lmt.common.modelcaps.PlayerModelCaps;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.EnumHandSide;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -40,7 +37,8 @@ public class ModelLittleMaidMultiModel extends ModelBase {
 	@Override
 	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn) {
 		
-		EntityCapsLiving caps = getModelCaps((EntityPlayer) entityIn);
+		//Player用ModelCaps
+		PlayerModelCaps caps = getModelCaps((EntityPlayer) entityIn);
 
 		ModelMultiBase modelMain = this.textureBox.models[0];
 		
@@ -54,7 +52,8 @@ public class ModelLittleMaidMultiModel extends ModelBase {
 	@Override
 	public void setLivingAnimations(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTickTime) {
 
-		EntityCapsLiving caps = getModelCaps((EntityPlayer) entitylivingbaseIn);
+		//Player用ModelCaps
+		PlayerModelCaps caps = getModelCaps((EntityPlayer) entitylivingbaseIn);
 
 		ModelMultiBase modelMain = this.textureBox.models[0];
 		
@@ -65,8 +64,8 @@ public class ModelLittleMaidMultiModel extends ModelBase {
 	@Override
 	public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale)
 	{
-		//Entity用ModelCapsでテスト
-		EntityCapsLiving caps = getModelCaps((EntityPlayer) entityIn);
+		//Player用ModelCaps
+		PlayerModelCaps caps = getModelCaps((EntityPlayer) entityIn);
 		
 		ModelMultiBase modelMain = this.textureBox.models[0];
 		modelMain.render(caps, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, true);
@@ -80,84 +79,14 @@ public class ModelLittleMaidMultiModel extends ModelBase {
 	 * @param player
 	 * @return
 	 */
-	protected EntityCapsLiving getModelCaps(EntityPlayer player) {
+	protected PlayerModelCaps getModelCaps(EntityPlayer player) {
 		
-		EntityCapsLiving caps = new EntityCapsLiving(player);
+		PlayerModelCaps caps = new PlayerModelCaps(player);
 		
-		//腕振り情報
-		float onGrounds[] = getOnGrounds(player);
-		
-		//パラメータを渡す
-		ModelMultiBase modelMain = this.textureBox.models[0];
-		
-		modelMain.setCapsValue(IModelCaps.caps_heldItemLeft, (Integer)0);
-		modelMain.setCapsValue(IModelCaps.caps_heldItemRight, (Integer)0);
-		modelMain.setCapsValue(IModelCaps.caps_onGround,
-				onGrounds[0],
-				onGrounds[1]);
-		modelMain.setCapsValue(IModelCaps.caps_isRiding, caps.getCapsValue(IModelCaps.caps_isRiding));
-		modelMain.setCapsValue(IModelCaps.caps_isSneak, caps.getCapsValue(IModelCaps.caps_isSneak));
-		modelMain.setCapsValue(IModelCaps.caps_aimedBow, false);
-		modelMain.setCapsValue(IModelCaps.caps_isWait, false);
-		modelMain.setCapsValue(IModelCaps.caps_isChild, false);
-		modelMain.setCapsValue(IModelCaps.caps_entityIdFactor, 0.0F);
-		modelMain.setCapsValue(IModelCaps.caps_ticksExisted, player.ticksExisted);
-		modelMain.setCapsValue(IModelCaps.caps_dominantArm, 0);
+		//モデルにCaps情報を設定する
+		caps.setModelMultiBaseCapsFromModelCaps(this.textureBox.models[0]);
 		
 		return caps;
-	}
-	
-	/**
-	 * メイドさんのmstatSwingStatusを仮想で再現
-	 * @param player
-	 * @return
-	 */
-	protected float[] getOnGrounds(EntityPlayer player) {
-		
-		float onGrounds[] = new float[] {0.0F, 0.0F};
-		
-		//利き腕取得
-		EnumHandSide dominantHand = player.getPrimaryHand();
-		boolean isDominantRight = dominantHand == EnumHandSide.RIGHT;
-		
-		//右か左かの判断
-		boolean isMainHand = EnumHand.MAIN_HAND == player.swingingHand;
-		
-		//左利きの場合は左右逆転する
-		if (!isDominantRight) {
-			isMainHand = !isMainHand;
-		}
-		
-		//腕振り
-		/*　tick単位での腕振り制御位置
-		腕を振った時にplayer.swingProgressにこの値が設定される
-		0.16666667
-		0.16666667
-		0.16666667
-		0.33333334
-		0.33333334
-		0.33333334
-		0.5
-		0.5
-		0.5
-		0.6666667
-		0.6666667
-		0.6666667
-		0.8333333
-		0.8333333
-		0.8333333
-		*/
-		if (isMainHand) {
-			//右振り
-			onGrounds[0] = player.swingProgress;
-			onGrounds[1] = 0.0F;
-		} else {
-			//左振り
-			onGrounds[0] = 0.0F;
-			onGrounds[1] = player.swingProgress;
-		}
-		
-		return onGrounds;
 	}
 	
 }
---
+++ src/main/java/net/firis/lmt/common/modelcaps/PlayerModelCaps.java
@@ -0,0 +1,325 @@
+package net.firis.lmt.common.modelcaps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import net.blacklab.lmr.entity.maidmodel.IModelCaps;
+import net.blacklab.lmr.entity.maidmodel.ModelMultiBase;
+import net.blacklab.lmr.util.helper.ItemHelper;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumHandSide;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+
+/**
+ * IModelCapsのEntityPlayer用
+ * @author firis-games
+ *
+ */
+public class PlayerModelCaps implements IModelCaps {
+	
+	private final EntityPlayer owner;
+	
+	public PlayerModelCaps(EntityPlayer player) {
+		this.owner = player;
+	}
+	
+	@Deprecated
+	@Override
+	public Map<String, Integer> getModelCaps() {
+		return null;
+	}
+
+	/**
+	 * set処理は利用しない
+	 */
+	@Override
+	public boolean setCapsValue(int pIndex, Object... pArg) {
+		return false;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Object getCapsValue(int pIndex, Object... pArg) {
+		switch (pIndex) {
+		case caps_Entity:
+			return owner;
+		case caps_health:
+			int iCapsHealth = (int) (owner.getHealth() / owner.getMaxHealth() * 20F);
+			return Math.min(iCapsHealth, 20);
+		case caps_healthFloat:
+			float fCapsHealth = (owner.getHealth() / owner.getMaxHealth() * 20F);
+			return Math.min(fCapsHealth, 20F);
+		case caps_ticksExisted:
+			return owner.ticksExisted;
+		case caps_heldItems:
+		case caps_currentEquippedItem:
+			ItemStack mainhand = owner.getHeldItemMainhand();
+			if (mainhand.isEmpty()) mainhand = null;
+			return mainhand;
+		case caps_currentArmor:
+			ItemStack aromor = ((List<ItemStack>)owner.getArmorInventoryList()).get((Integer) pArg[0]);
+			if (aromor.isEmpty()) aromor = null;
+			return aromor;
+		case caps_posX:
+			return owner.posX;
+		case caps_posY:
+			return owner.posY;
+		case caps_posZ:
+			return owner.posZ;
+		case caps_pos:
+			if (pArg == null) {
+				return new Double[] {owner.posX, owner.posY, owner.posZ};
+			}
+			return (Integer)pArg[0] == 0 ? owner.posX : (Integer)pArg[0] == 1 ? owner.posY : owner.posZ;
+		case caps_motionX:
+			return owner.motionX;
+		case caps_motionY:
+			return owner.motionY;
+		case caps_motionZ:
+			return owner.motionZ;
+		case caps_motion:
+			if (pArg == null) {
+				return new Double[] {owner.motionX, owner.motionY, owner.motionZ};
+			}
+			return (Integer)pArg[0] == 0 ? owner.motionX : (Integer)pArg[0] == 1 ? owner.motionY : owner.motionZ;
+
+		case caps_rotationYaw:
+			return owner.rotationYaw;
+		case caps_rotationPitch:
+			return owner.rotationPitch;
+		case caps_prevRotationYaw:
+			return owner.prevRotationYaw;
+		case caps_prevRotationPitch:
+			return owner.prevRotationPitch;
+		case caps_renderYawOffset:
+			return owner.renderYawOffset;
+		case caps_onGround:
+			return this.getOnGrounds();
+		case caps_isRiding:
+			return owner.isRiding();
+		case caps_isRidingPlayer:
+			return false;
+		case caps_isChild:
+			return owner.isChild();
+		case caps_isWet:
+			return owner.isWet();
+		case caps_isDead:
+			return owner.isDead;
+		case caps_isJumping:
+			return false;//owner.isJumping;
+		case caps_isInWeb:
+			return false;//owner.isInWeb;
+		case caps_isSwingInProgress:
+			return owner.isSwingInProgress;
+		case caps_isSneak:
+			return owner.isSneaking();
+		case caps_isBurning:
+			return owner.isBurning();
+		case caps_isInWater:
+			return owner.isInWater();
+		case caps_isInvisible:
+			return owner.isInvisible();
+		case caps_isSprinting:
+			return owner.isSprinting();
+		case caps_PosBlockID:
+			return owner.getEntityWorld().getBlockState(new BlockPos(
+					MathHelper.floor(owner.posX + (Double)pArg[0]),
+					MathHelper.floor(owner.posY + (Double)pArg[1]),
+					MathHelper.floor(owner.posZ + (Double)pArg[2]))).getBlock();
+		case caps_PosBlockState:
+			return owner.getEntityWorld().getBlockState(new BlockPos(
+					MathHelper.floor(owner.posX + (Double)pArg[0]),
+					MathHelper.floor(owner.posY + (Double)pArg[1]),
+					MathHelper.floor(owner.posZ + (Double)pArg[2])));
+		case caps_PosBlockAir:
+			
+			IBlockState state = owner.getEntityWorld().getBlockState(new BlockPos(
+					MathHelper.floor(owner.posX + (Double)pArg[0]),
+					MathHelper.floor(owner.posY + (Double)pArg[1]),
+					MathHelper.floor(owner.posZ + (Double)pArg[2])));
+			
+			return !state.getBlock().causesSuffocation(state);
+			
+		case caps_PosBlockLight:
+			return owner.getEntityWorld().getBlockLightOpacity(new BlockPos(
+					MathHelper.floor(owner.posX + (Double)pArg[0]),
+					MathHelper.floor(owner.posY + (Double)pArg[1]),
+					MathHelper.floor(owner.posZ + (Double)pArg[2])));
+		case caps_PosBlockPower:
+			return owner.getEntityWorld().getStrongPower(new BlockPos(
+					MathHelper.floor(owner.posX + (Double)pArg[0]),
+					MathHelper.floor(owner.posY + (Double)pArg[1]),
+					MathHelper.floor(owner.posZ + (Double)pArg[2])));
+		case caps_boundingBox:
+			if (pArg == null) {
+				return owner.getEntityBoundingBox();
+			}
+			switch ((Integer)pArg[0]) {
+			case 0:
+				return owner.getEntityBoundingBox().maxX;
+			case 1:
+				return owner.getEntityBoundingBox().maxY;
+			case 2:
+				return owner.getEntityBoundingBox().maxZ;
+			case 3:
+				return owner.getEntityBoundingBox().minX;
+			case 4:
+				return owner.getEntityBoundingBox().minY;
+			case 5:
+				return owner.getEntityBoundingBox().minZ;
+			}
+		case caps_isLeeding:
+			return false;
+		case caps_getRidingName:
+			return owner.getRidingEntity() == null ? "" : EntityList.getEntityString(owner.getRidingEntity());
+
+		// World
+		case caps_WorldTotalTime:
+			return owner.getEntityWorld().getWorldInfo().getWorldTotalTime();
+		case caps_WorldTime:
+			return owner.getEntityWorld().getWorldInfo().getWorldTime();
+		case caps_MoonPhase:
+			return owner.getEntityWorld().getMoonPhase();
+		case caps_TextureEntity:
+			return owner;
+			
+		//弓構え
+		case caps_aimedBow:
+			ItemStack mainStack = owner.getHeldItemMainhand();
+            ItemStack offStack = owner.getHeldItemOffhand();
+            //手持ちアイテムあり かつ アイテム使用中
+            if (!mainStack.isEmpty() && owner.getItemInUseCount() > 0) {
+            	if (mainStack.getItemUseAction() == EnumAction.BOW) {
+            		return true;
+            	}
+            }
+            if (!offStack.isEmpty() && owner.getItemInUseCount() > 0) {
+            	if (offStack.getItemUseAction() == EnumAction.BOW) {
+            		return true;
+            	}
+            }
+			return false;
+			
+		//Entityごとの揺らぎを持たせているらしい？
+		case caps_entityIdFactor:
+			return 0.0F;
+			
+		//利き手
+		case caps_dominantArm:
+			return owner.getPrimaryHand() == EnumHandSide.RIGHT ? 0 : 1;
+		
+		//アイテムを持った時の腕振り制御（未使用・後方互換用）
+		case caps_heldItemLeft:
+		case caps_heldItemRight:
+			return 0;
+			
+		//メイドさん待機モーション
+		case caps_isWait:
+			return false;
+			
+		//砂糖を持った時の首傾げ
+		case caps_isLookSuger:
+			return ItemHelper.isSugar(this.owner.getHeldItemMainhand()); 
+		}
+
+		return null;
+	}
+	
+	private List<Integer> modelCapsList = this.initModelCapsList();
+	private List<Integer> initModelCapsList() {
+		List<Integer> caps = new ArrayList<>();
+		
+		//モデル側へ受け渡すcapsIdを設定
+		caps.add(IModelCaps.caps_heldItemLeft);
+		caps.add(IModelCaps.caps_heldItemRight);
+		caps.add(IModelCaps.caps_onGround);
+		caps.add(IModelCaps.caps_isRiding);
+		caps.add(IModelCaps.caps_isSneak);
+		caps.add(IModelCaps.caps_aimedBow);
+		caps.add(IModelCaps.caps_isWait);
+		caps.add(IModelCaps.caps_isChild);
+		caps.add(IModelCaps.caps_entityIdFactor);
+		caps.add(IModelCaps.caps_ticksExisted);
+		caps.add(IModelCaps.caps_dominantArm);
+		
+		return caps;
+	}
+	
+	/**
+	 * ModelCapsの情報をModelBaseへ反映する
+	 */
+	public void setModelMultiBaseCapsFromModelCaps(ModelMultiBase model) {
+		
+		for (Integer capsId : modelCapsList) {
+			//onGroundだけ特殊処理
+			if (IModelCaps.caps_onGround == capsId) {
+				float[] onGround = (float[]) this.getCapsValue(capsId);
+				model.setCapsValue(capsId, onGround[0], onGround[1]);
+			} else {
+				model.setCapsValue(capsId, this.getCapsValue(capsId));
+			}
+		}
+		
+	}
+	
+	
+	/**
+	 * メイドさんのmstatSwingStatusを仮想で再現
+	 * @param player
+	 * @return
+	 */
+	protected float[] getOnGrounds() {
+		
+		float onGrounds[] = new float[] {0.0F, 0.0F};
+		
+		//利き腕取得
+		EnumHandSide dominantHand = this.owner.getPrimaryHand();
+		boolean isDominantRight = dominantHand == EnumHandSide.RIGHT;
+		
+		//右か左かの判断
+		boolean isMainHand = EnumHand.MAIN_HAND == this.owner.swingingHand;
+		
+		//左利きの場合は左右逆転する
+		if (!isDominantRight) {
+			isMainHand = !isMainHand;
+		}
+		
+		//腕振り
+		/*　tick単位での腕振り制御位置
+		腕を振った時にplayer.swingProgressにこの値が設定される
+		0.16666667
+		0.16666667
+		0.16666667
+		0.33333334
+		0.33333334
+		0.33333334
+		0.5
+		0.5
+		0.5
+		0.6666667
+		0.6666667
+		0.6666667
+		0.8333333
+		0.8333333
+		0.8333333
+		*/
+		if (isMainHand) {
+			//右振り
+			onGrounds[0] = this.owner.swingProgress;
+			onGrounds[1] = 0.0F;
+		} else {
+			//左振り
+			onGrounds[0] = 0.0F;
+			onGrounds[1] = this.owner.swingProgress;
+		}
+		
+		return onGrounds;
+	}
+	
+}
