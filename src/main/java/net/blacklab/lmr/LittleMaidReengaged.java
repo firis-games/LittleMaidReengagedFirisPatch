bac812a2b9d7c34b1824274b7e3a39df02a78a63
firis <firis.games.lapismoonlight@gmail.com>
2020-05-05 19:06:17 +0900

    ver.fp.035

--- src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
+++ src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
@@ -75,7 +75,7 @@ public class LittleMaidReengaged {
 
 	public static final String DOMAIN = "lmreengaged";
 	public static final String NAME = "LittleMaidReengaged";
-	public static final String VERSION = "9.0.0.fp.034";
+	public static final String VERSION = "9.0.1.fp.035";
 	public static final String ACCEPTED_MCVERSION = "[1.12.2]";
 	public static final String DEPENDENCIES = "required-after:forge@[1.12.2-14.23.5.2768,);";
 
