20f65eb1daac18e079484f8e5170333510e36477
firis <firis.games.lapismoonlight@gmail.com>
2020-05-17 15:40:44 +0900

    LMアバターの防具表示条件追加
    ・防具装備時にモデルを表示する条件追加

--- src/main/java/net/firis/lmt/client/renderer/layer/LayerArmorLMAvatar.java
+++ src/main/java/net/firis/lmt/client/renderer/layer/LayerArmorLMAvatar.java
@@ -22,6 +22,28 @@ public class LayerArmorLMAvatar extends LayerArmorLittleMaidBase {
 		super(rendererIn);
 	}
 	
+	/**
+	 * 防具の描画判定
+	 * @param entityLivingBaseIn
+	 * @param limbSwing
+	 * @param limbSwingAmount
+	 * @param partialTicks
+	 * @param ageInTicks
+	 * @param netHeadYaw
+	 * @param headPitch
+	 * @param scale
+	 * @param slot
+	 * @return
+	 */
+	@Override
+	protected boolean isRenderArmorLayer(EntityLivingBase entityLivingBaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale, EntityEquipmentSlot slot) {
+		//対象スロットの描画対象チェック
+		EntityPlayer player = (EntityPlayer) entityLivingBaseIn;
+		if (player.inventory.armorItemInSlot(slot.getIndex()).isEmpty()) return false;
+		
+		return true;
+	}
+	
 	@Override
 	protected IModelConfigCompound getArmorModelConfigCompound(EntityLivingBase entityLivingBaseIn, float limbSwing,
 			float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale,
