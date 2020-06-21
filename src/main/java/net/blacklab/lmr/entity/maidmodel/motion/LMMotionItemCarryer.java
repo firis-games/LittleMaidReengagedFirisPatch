89b6a6e54797cd2d0f8cd0ef60f777ae061da9e7
firis <firis.games.lapismoonlight@gmail.com>
2020-06-21 20:08:17 +0900

    アイテム持ち運びモーション調整
    ・新しいIModelCapsに合わせてモーション処理を調整

--- src/main/java/net/blacklab/lmr/entity/maidmodel/motion/LMMotionItemCarryer.java
+++ src/main/java/net/blacklab/lmr/entity/maidmodel/motion/LMMotionItemCarryer.java
@@ -1,11 +1,10 @@
 package net.blacklab.lmr.entity.maidmodel.motion;
 
 import firis.lmmm.api.caps.IModelCaps;
+import firis.lmmm.api.caps.ModelCapsHelper;
 import firis.lmmm.api.model.ModelLittleMaidBase;
 import firis.lmmm.api.model.motion.ILMMotion;
-import net.blacklab.lmr.entity.littlemaid.EntityLittleMaid;
 import net.blacklab.lmr.entity.littlemaid.mode.EntityMode_Basic;
-import net.minecraft.entity.Entity;
 
 /**
  * 追加モーション
@@ -21,19 +20,13 @@ public class LMMotionItemCarryer implements ILMMotion {
 	public boolean postRotationAngles(ModelLittleMaidBase model, String motion, float limbSwing, float limbSwingAmount,
 			float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, IModelCaps pEntityCaps) {
 		
-		Entity entity = (Entity) pEntityCaps.getCapsValue(IModelCaps.caps_Entity);
-		
-		if (!(entity instanceof EntityLittleMaid)) return false;
-		
-		EntityLittleMaid maid = (EntityLittleMaid) entity;
-		
-		String job = maid.jobController.getMaidModeString();
+		String job = ModelCapsHelper.getCapsValueString(pEntityCaps, IModelCaps.caps_job);
 		boolean idDisplay = false;
-    	if (EntityMode_Basic.mmode_FarmPorter.equals(job) 
-    			|| EntityMode_Basic.mmode_LumberjackPorter.equals(job)
-    			|| EntityMode_Basic.mmode_RipperPorter.equals(job)
-    			|| EntityMode_Basic.mmode_SugarCanePorter.equals(job)
-    			|| EntityMode_Basic.mmode_AnglerPorter.equals(job)) {
+    	if (EntityMode_Basic.mmode_FarmPorter.toLowerCase().equals(job) 
+    			|| EntityMode_Basic.mmode_LumberjackPorter.toLowerCase().equals(job)
+    			|| EntityMode_Basic.mmode_RipperPorter.toLowerCase().equals(job)
+    			|| EntityMode_Basic.mmode_SugarCanePorter.toLowerCase().equals(job)
+    			|| EntityMode_Basic.mmode_AnglerPorter.toLowerCase().equals(job)) {
     		idDisplay = true;
     	}
     	
